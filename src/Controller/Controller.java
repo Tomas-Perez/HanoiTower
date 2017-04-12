ff63e6d1f4802b9dfed9456b8338c1cbde61feb3
Agustin Bettati <bettatiagustin@gmail.com>
2017-04-12 15:56:51 -0300

    Added controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,26 @@
 package Controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Stack;
+import Model.Model;
+import View.View;
+
 public class Controller {
+    private Model solver;
+    private View view;
+
+    public Controller(){
+        view = new View(new NextStep());
+        solver = new Model();
+    }
+
+    public class NextStep implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            ArrayList<Stack<Integer>> discs = solver.getDiscs();
+            view.display(discs);
+        }
+    }
 }
